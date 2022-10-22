b881ce3ed073cfe9963092c52eb7918cf4bab32e
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-22 16:54:36 +0500

    14

--- basic_func14.py
+++ basic_func14.py
@@ -1,3 +1,12 @@
-# Create a function called main.
-# Create function arguments a and b.
-# Returns the floor division of a and b.
+# from math import floor
+
+
+from math import floor
+def main(a,b):
+    x = a/b
+    return(floor(x))
+
+print((main(11,2)))
+
+
+
