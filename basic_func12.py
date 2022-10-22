ea654bf654234f3c33c407333595d4166af7ab58
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-22 16:19:26 +0500

    12

--- basic_func12.py
+++ basic_func12.py
@@ -2,3 +2,9 @@
 # Create function argument a.
 # Return the value of a.
 # Round the result to 2 decimal places.
+#from shutil import register_unpack_format
+
+
+def main (a):
+    return(round(a,2))
+print((main(6.453)))
