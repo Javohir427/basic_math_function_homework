2f4206393e55a4a676a4388754d687319bf11dc9
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-23 09:39:54 +0500

    15

--- basic_func15.py
+++ basic_func15.py
@@ -1,3 +1,11 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the modulus of a and b.
+#from __future__ import print_function
+
+
+def main(a,b):
+    x = a%b
+    return(x)
+ 
+print(main(23,5))
