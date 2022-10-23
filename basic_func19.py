13f5120eef75caeea679cb818a61e778a4d30da4
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-23 10:06:06 +0500

    19

--- basic_func19.py
+++ basic_func19.py
@@ -1,3 +1,8 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
+#from math import abs
+#from msilib import PID_TITLE 
+def main(a,b):
+    return(abs(a-b))
+print(main(4,11))
