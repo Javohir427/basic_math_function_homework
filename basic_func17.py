c0ffdb42f788c90f8a697cd6a27e01fe2cbe5f02
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-10-23 09:51:44 +0500

    17

--- basic_func17.py
+++ basic_func17.py
@@ -1,5 +1,8 @@
-
+#from math import pi
+#from traceback import print_tb
+#from xml.sax.handler import property_interning_dict
+#from math import pi
+#from turtle import clear
 def main(a):
-    a = pi
-    return (a)
-print((str(main(pi))))
+    return((a))
+print((main('pi')))
